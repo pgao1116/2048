0b4c024a2e9ccdf150d050ff0c7f2a82419f93d0
shale <slucas001@citymail.cuny.edu>
2024-04-20 11:21:35 -0400

    added source file for webserver

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket; 
+
+// Adds a route
+#[get("/")]
+fn index () -> &'static str {
+	"get request"
 }
+
+// Launches web server
+#[launch]
+fn rocket() -> _ {
+	rocket::build().mount("/", routes![index])
+}
+
