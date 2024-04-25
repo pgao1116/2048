e8ab13a6d3be0f0a833cf83b64241e7232db9d90
shale <slucas001@citymail.cuny.edu>
2024-04-25 00:28:42 -0400

    added cases for keystrokes, and started shift func

--- game/src/board.rs
+++ game/src/board.rs
@@ -5,9 +5,9 @@ use rocket::serde::json::Json;
 pub enum KeyStroke {
 	NO_KEY, 				// No key pressed
 	KEY_LEFT, 
-	KEY_RIGHT, 
+	KEY_RIGHT, 				// ... Right arrow pressed
 	KEY_DOWN, 
-	KEY_UP,					// Up arrow pressed
+	KEY_UP,					// ... Up arrow pressed
 }
 
 pub struct Board {
@@ -15,24 +15,62 @@ pub struct Board {
 	game_over: bool, 		 
 	moves: i64, 			// The number of moves (key's pressed)	
 	keystroke: KeyStroke	// The current keystroke
+	score: i64, 			
 } 
 
-
-
-// Moves either KEY_UP, KEY_RIGHT, KEY_LEFT, or KEY_DOWN
 impl Board {
+
+	// Performs a shift to the left 
+	// i.e. let l = [4, 0, 0, 4], then shift(l) -> [4, 4, 0, 0].
+	// do shift(l) again, and I get [8, 0, 0, 0], buggy as of now. 
+	fn shift(list :&mut Vec<usize>) {
+		let mut left: usize = 0; 
+		let mut right: usize = left + 1; 
+
+		for _ in 0..3 {
+			if list[left] == list[right] {
+				list[left] += list[right]; 
+				list[right] = 0; 	
+			} 
+		} 	
+	}
 	
-	// The 2048 game 
-	pub fn process_key(&self, key: KeyStroke) {
+	// Updates the board 
+	pub fn update_board(&self, key: KeyStroke) {
 		
-		for i in 0..self.mat.length() {
-			for j in 0..self.mat[i].length() {
-		
+		for i in 0..self.mat.len() {
+			for j in 0..self.mat[i].len() {
+				
+				match key {
+					NO_KEY => continue, 
+
+					KEY_LEFT =>  {
+
+
+					}, 
+				
+					KEY_RIGHT => {
+
+					}, 
+				
+					KEY_UP => {
+
+					}, 
+						
+					KEY_DOWN => {
+
+					}, 
+ 				} 		
 			} 
 		} 	
 
 	
 	}
+
+
+	pub fn terminate_game() { 
+
+	} 
  
 } 
 
