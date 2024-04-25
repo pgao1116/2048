d1c773869177aa77dac528a791a67f0703fb3ecc
shale <slucas001@citymail.cuny.edu>
2024-04-25 00:48:16 -0400

    something of a js file

--- 2048.js
+++ 2048.js
@@ -1,3 +1,7 @@
-console.log("hello world"); 
+// https://www.topcoder.com/thrive/articles/fetch-api-javascript-how-to-make-get-and-post-requests
 
-console.log ("Testing");
+endpoint = "localhost:8080/test"
+fetch(endpoint)
+	.then(response => {
+		console.log(response); 
+	})
