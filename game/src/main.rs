7881b62c6ee3af7d95d40d3a54d0ec38a92c40b9
shale <slucas001@citymail.cuny.edu>
2024-04-25 00:30:05 -0400

    added some more ideas of server and game logic

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,14 +1,60 @@
 #[macro_use] extern crate rocket; 
+use rocket::serde::json::Json; 
+use board::Board; 
 
-// Adds a route
+// Serve HTML
 #[get("/")]
-fn index () -> &'static str {
-	"get request"
+fn index() -> Json() {
+	
+	let const filepath = "../../2048.html"
+	let serving = rocket::fs::NamedFile(filepath); 
+	Json(serving)
 }
 
-// Launches web server
+// Returns JSON of the new game state
+#[get("/gamestate"), ("")]
+fn handle_get(board: &Board) -> _ { 
+	
+	match () {
+		Some() => 
+		None => 	
+	}
+	
+	Json();  
+} 
+
+// Takes user's keystroke and updates board
+#[post("/keystroke")]
+fn handle_post(data: KeyStroke, board: &board) {
+
+	// Do somthing to update the state of the game	
+
+	board.moves = board.moves + 1; 	
+	if !board.game_terminates() {
+		board.process_key(data); 
+	} else {
+		return Json(board.final_state()); 
+	} 
+}
+
+// Returns 404 error msg
+#[catch(404)]
+fn not_found() -> ... {	
+	// Do something like this...
+	let error = "Error route not supported by the server."; 
+	return error; 
+}
+
 #[launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
+	
+	// Do something with the board
+	let mut board = Board::new(); 
+
+	// Serve's HTML, and waits for GET and POST requests
+	rocket::build()
+		.mount("/", routes![index])
+		.mount("/keystroke", routes![index])
+		.mount("/gamestate", routes![index]); 
 }
 
