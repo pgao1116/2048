2914350ec466ca4412267c29a35be440b6fa449f
shale <slucas001@citymail.cuny.edu>
2024-06-18 23:40:21 -0400

    finally compiles

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,37 +1,41 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
-use rocket_contrib::json::Json;
-use serde::Deserialize;
+
+use rocket::serde::json::Json;
+use rocket::fs::NamedFile;
+use rocket::State;
 
 mod board;
+use board::Board;
+use board::KeyStroke;
 
 // Serve HTML
 #[get("/")]
-fn index() -> rocket::fs::NamedFile {	
-	const filepath: &str = "./static/2048.html";
-	let serving = rocket::fs::NamedFile(filepath); 
-	return serving;
+async fn index() -> NamedFile {	
+	// const filepath: &str = "./static/2048.html";
+	// let serving = rocket::fs::NamedFile(filepath); 
+	// return serving;
+
+	NamedFile::open("../static/2048.html").await.unwrap()
 }
 
 // Returns JSON of the new game state
 #[get("/gamestate")]
-fn handle_get(board: Option<& mut Board>) -> Json(Board) { 
-	Json(board.unwrap())
+fn handle_get(board: &State<Board>) -> Json<Board> { 
+	Json(board.inner().clone())
 } 
 
 // Waiting for user's keystroke and then returns
 #[post("/keystroke", format="json", data="<data>")]
-async fn handle_post(data: KeyStroke, mut board: Option<& mut Board>) ->Json<Board> {
+fn handle_post(data: Json<KeyStroke>, board: &State<Board>) ->Json<Board> {
+
+	let mut board = board.inner().clone();
 
 	if !board.is_over() {
-		board.update(data); 
-		board.moves = board.moves + 1; 
-		board.score = board.score + ...; 
-		
-	} else {
-		return Json(board.unwrap()); 
+		board.update_board(data.into_inner()); 	
 	} 
+
+	Json(board.clone())
 }
 
 // Returns 404 error msg
@@ -44,12 +48,14 @@ fn not_found() -> &'static str {
 fn rocket() -> _ {
 	
 	// Do something with the board
-	let mut board = Board::new();
+	let board = Board::new();
 
 	// Serve's HTML, and waits for GET and POST requests
 	rocket::build()
 		.mount("/", routes![index])
-		.mount("/keystroke", routes![index])
-		.mount("/gamestate", routes![index])
+		.mount("/keystroke", routes![handle_post])
+		.mount("/gamestate", routes![handle_get])
+		.register("/", catchers![not_found])
+		.manage(board)
 }
 
