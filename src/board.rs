1da1308be8fe5ff22446b931a94f48abf8cea0ed
shale <slucas001@citymail.cuny.edu>
2024-06-28 00:44:55 -0400

    added debug trait to keystroke, and removeed unecessary for loops in update_board

--- src/board.rs
+++ src/board.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 
-#[derive(Deserialize, Serialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 #[serde(crate = "rocket::serde")]
 pub enum KeyStroke {
 	NoKey = 0, 				// No key pressed
@@ -75,100 +75,93 @@ impl Board {
 	
 	// Updates the board, using shift
 	pub fn update_board(& mut self, key: KeyStroke) {
+			
+		match key {
+			KeyStroke::NoKey => return, 
+
+			KeyStroke::KeyLeft =>  {
+				// Copies row of the board into a Vec
+				for i in 0..4 {
+					let mut row: Vec<usize> = Vec::new(); 
+					for j in 0..4 {
+						row.push(self.mat[i][j] as usize); 
+					} 	
+				
+					// Buggy code below, need shift to be called once!! 	
+					// Does a left-shift, and copies the row back into the board
+					self.score += Self::shift(&mut row); 
+					self.score += Self::shift(&mut row); 	
+					
+					for j in 0..4 { 
+						self.mat[i][j] = row[j] as i32; 
+					} 
+				} 
+			}, 
 		
-		for i in 0..self.mat.len() {
-			for _j in 0..self.mat[i].len() {
-				
-				match key {
-					KeyStroke::NoKey => continue, 
-
-					KeyStroke::KeyLeft =>  {
-						// Copies row of the board into a Vec
-						for i in 0..4 {
-							let mut row: Vec<usize> = Vec::new(); 
-							for j in 0..4 {
-								row.push(self.mat[i][j] as usize); 
-							} 	
-						
-							// Buggy code below, need shift to be called once!! 	
-							// Does a left-shift, and copies the row back into the board
-							self.score += Self::shift(&mut row); 
-							self.score += Self::shift(&mut row); 	
-							
-							for j in 0..4 { 
-								self.mat[i][j] = row[j] as i32; 
-							} 
-						} 
-
-						self.moves += 1; // Update the moves at the end
-					}, 
-				
-					KeyStroke::KeyRight => {	
-						// Copies row of the board into a Vec
-						for i in 0..4 {
-							let mut row: Vec<usize> = Vec::new(); 
-							for j in 0..4 {
-								row.push(self.mat[i][j] as usize); 
-							} 	
-							// By reversing the array, shifting left, and reversing again, 
-							// the shift function, can be repurposed for a right-shift.	
-							row.reverse();
-							self.score += Self::shift(&mut row); 
-							self.score += Self::shift(&mut row); 	
-							row.reverse();
-							
-							for j in 0..4 { 
-								self.mat[i][j] = row[j] as i32;
-							} 
-						} 
-						
-						self.moves += 1;
-					}, 
-				
-					KeyStroke::KeyUp => {
+			KeyStroke::KeyRight => {	
+				// Copies row of the board into a Vec
+				for i in 0..4 {
+					let mut row: Vec<usize> = Vec::new(); 
+					for j in 0..4 {
+						row.push(self.mat[i][j] as usize); 
+					} 	
+					// By reversing the array, shifting left, and reversing again, 
+					// the shift function, can be repurposed for a right-shift.	
+					row.reverse();
+					self.score += Self::shift(&mut row); 
+					self.score += Self::shift(&mut row); 	
+					row.reverse();
 					
-						// Copies the columns into a Vec	
-						for i in 0..4 {
-							let mut col: Vec<usize> = Vec::new(); 	
-							for j in 0..4 {
-								col.push(self.mat[j][i] as usize); 
-							} 
-					
-							// Does a left-shift, and copies the col back into the board
-							self.score += Self::shift(&mut col); 
-							self.score += Self::shift(&mut col); 	
+					for j in 0..4 { 
+						self.mat[i][j] = row[j] as i32;
+					} 
+				} 
+				
+			},
+		
+			KeyStroke::KeyUp => {
+			
+				// Copies the columns into a Vec	
+				for i in 0..4 {
+					let mut col: Vec<usize> = Vec::new(); 	
+					for j in 0..4 {
+						col.push(self.mat[j][i] as usize); 
+					} 
+			
+					// Does a left-shift, and copies the col back into the board
+					self.score += Self::shift(&mut col); 
+					self.score += Self::shift(&mut col); 	
 
-								
-							for j in 0..4 {
-								self.mat[i][j] = col[i] as i32;
-							} 	
-						}
-							self.moves += 1;
-					},
 						
-					KeyStroke::KeyDown => {
+					for j in 0..4 {
+						self.mat[i][j] = col[i] as i32;
+					} 	
+				}
+			},
+				
+			KeyStroke::KeyDown => {
 
-						// Copies the columns into a Vec	
-						for i in 0..4 {
-							let mut col: Vec<usize> = Vec::new(); 	
-							for j in 0..4 {
-								col.push(self.mat[j][i] as usize); 
-							} 
-							
-							col.reverse(); 	
-							self.score += Self::shift(&mut col); 
-							self.score += Self::shift(&mut col); 	
-							col.reverse(); 	
-							
-							for j in 0..4 {
-								self.mat[i][j] = col[i] as i32; 
-							} 	
-						}
-							self.moves += 1;
- 					}, 		
-				}	// match statement	 
-			} 	
-		}	
+				// Copies the columns into a Vec	
+				for i in 0..4 {
+					let mut col: Vec<usize> = Vec::new(); 	
+					for j in 0..4 {
+						col.push(self.mat[j][i] as usize); 
+					} 
+					
+					col.reverse(); 	
+					self.score += Self::shift(&mut col); 
+					self.score += Self::shift(&mut col); 	
+					col.reverse(); 	
+					
+					for j in 0..4 {
+						self.mat[i][j] = col[i] as i32; 
+					} 	
+				}
+			}, 		
+
+		}	// match statement	 
+		
+		self.moves += 1;
 	} // end function
 	
 	
